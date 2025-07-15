3d0c574c2f4dc1958c69ed70959699a603705eb3
Julio Caro <jcarocota@gmail.com>
2025-07-15 14:26:47 -0700

    Este es mi primer bulk de información

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+/*function MyButton(props) {
+  return(
+    <button>{props.label}</button>
+  );
+};
+
+export default function App() {
+  return (
+    <MyButton label="Mi primer botón!" />
+  );
+};*/
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[anfibios, setAnfibios] = useState([]);
+  
+  useEffect(
+    () => {
+      fetch("/amphibians")
+      .then(response => response.json())
+      .then(data => setAnfibios(data))
+      .catch(error => console.log("Error: ", error));
+    }, []
   );
+
+  return (
+    <Container className='py-4'>
+      <h1 className='text-center mb-4'>Lista de Anfibios</h1>
+      <Row xs={1} md={2} lg={3} className='g-4'>
+        {
+          anfibios.map(
+            (anfibio, idx) => (
+              <Col key={idx}>
+                <Card>
+                  
+                  <Card.Body>
+                    <Card.Title>{anfibio.name}</Card.Title>
+                    <Card.Subtitle>{anfibio.type}</Card.Subtitle>
+                    <Card.Text>{anfibio.description}</Card.Text>
+                  </Card.Body>
+                  <Card.Img variant='bottom' src={anfibio.img_src} />
+                </Card>
+              </Col>
+            )
+          )
+        }        
+      </Row>
+
+    </Container>
+  );
+
 }
 
 export default App;
